182902c2379fdec3abc03bbf4c13a936351d54aa
kinosuke01 <kinosuke.nekonome@gmail.com>
2022-04-10 17:03:57 +0900

    update

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -8,6 +8,11 @@ fn main() {
     if_fn();
     while_fn();
     for_fn();
+
+    // TODO
+    // 温度を華氏と摂氏で変換する。
+    // フィボナッチ数列のn番目を生成する。
+    // クリスマスキャロルの定番、"The Twelve Days of Christmas"の歌詞を、 曲の反復性を利用して出力する。
 }
 
 fn tup_fn() {
