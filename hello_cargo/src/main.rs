6fd1fd6f6f88b0a37cbffd70752ca9ead20e21ce
kinosuke01 <kinosuke.nekonome@gmail.com>
2022-04-10 17:29:16 +0900

    update

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -9,10 +9,12 @@ fn main() {
     while_fn();
     for_fn();
 
-    // TODO
     // 温度を華氏と摂氏で変換する。
+    println!("{}", to_fahrenheit(10.0));
+    println!("{}", to_celsius(50.0));
+
     // フィボナッチ数列のn番目を生成する。
-    // クリスマスキャロルの定番、"The Twelve Days of Christmas"の歌詞を、 曲の反復性を利用して出力する。
+    println!("{}", fibonacci_of(10));
 }
 
 fn tup_fn() {
@@ -90,4 +92,20 @@ fn for_fn() {
         println!("{}", number);
     }
     println!("LIFTOFF!!!!!");
+}
+
+fn to_fahrenheit(x: f64) -> f64 {
+    x * 1.8 + 32.0
+}
+
+fn to_celsius(x: f64) -> f64 {
+    (x - 32.0) / 1.8
+}
+
+fn fibonacci_of(x: u32) -> u32 {
+    if x == 0 || x == 1{
+        x
+    } else {
+        fibonacci_of(x - 2) + fibonacci_of(x - 1)
+    }
 }
