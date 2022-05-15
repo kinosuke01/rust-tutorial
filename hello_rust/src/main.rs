55cbf08f9b91900544f89c9db4c5701b7917ab55
kinosuke01 <kinosuke.nekonome@gmail.com>
2022-05-15 10:56:58 +0900

    update

--- hello_rust/src/main.rs
+++ hello_rust/src/main.rs
@@ -1,4 +1,6 @@
 use std::fs::File;
+use std::io;
+use std::io::Read;
 use std::io::ErrorKind;
 
 fn main() {
@@ -655,4 +657,31 @@ fn error_fn() {
         }
     };
     println!("file info is {:?}", f);
+
+    // Errのときはすぐにpanicさせる場合
+    // let f = File::open("notfound.txt").unwrap();
+    // エラーメッセージを指定してpanicさせるとき
+    // let f = File::open("notfound.txt").expect("Failed to open file");
+
+    // エラーの移譲
+    // matchでエラーを呼び出しもとのコードに返す関数
+    fn read_username_from_file() -> Result<String, io::Error> {
+        let f = File::open("hello.txt");
+
+        let mut f = match f {
+            Ok(file) => file,
+            Err(e) => return Err(e)
+        };
+
+        let mut s = String::new();
+
+        match f.read_to_string(&mut s) {
+            Ok(_) => Ok(s),
+            Err(e) => Err(e),
+        }
+    }
+    match read_username_from_file() {
+        Ok(s) => println!("username is {}", s),
+        Err(e) => println!("error is {:?}", e),
+    }
 }
